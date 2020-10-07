b0dbeaa06e71d413ce83afab7d9e0a77c2528b34
Antonio Rv <antoniorp1998@gmail.com>
2020-10-07 10:50:36 +0200

    creada clase dentro de src (closes #7)

---
+++ src/app.js
@@ -0,0 +1,19 @@
+class Cancion {
+    constructor( acousticness, danceability, energy, instrumentalness, liveness, loudness, mode, popularity, speechiness, valence) {
+      this.acousticness = acousticness;
+      this.danceability = danceability;
+      this.energy = energy;
+      this.instrumentalness = instrumentalness;
+      this.liveness = liveness;
+      this.loudness = loudness;
+      this.mode = mode;
+      this.popularity = popularity;
+      this.speechiness = speechiness;
+      this.valence = valence;
+    }
+
+    obtenerPeticion() {
+    }
+
+
+  }
