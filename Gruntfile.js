a3d7f35ae12efb4102f6615c9413bf50549cb4db
Antonio Rv <antoniorp1998@gmail.com>
2020-11-10 17:14:10 +0100

    Cambiado el gestor de tareas, npm por grunt (closes #44)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,33 @@
 module.exports = function(grunt) {
-
+    
+    grunt.loadNpmTasks('grunt-run');
     // Project configuration.
     grunt.initConfig({
-      
+        run: {
+            install: {
+                cmd: 'npm',
+                args: [
+                  'install'
+                ]  
+            },
+            test: {
+              cmd: 'jest'
+            },
+            coverage: {
+              cmd: 'jest',
+              args: [
+                  '--coverage',
+                  '--coverageDirectory=\'docs/coverage\''
+              ]
+            }
+
+
+          }
     });
   
-  };
+  grunt.registerTask('default', [ 'run:install' ]);  
+  grunt.registerTask('install', [ 'run:install' ]);  
+  grunt.registerTask('test', [ 'run:test' ]);
+  grunt.registerTask('coverage', [ 'run:coverage' ]);
+
+};
