cc057b29d320665bda414850e81dc1cc5656c3e5
Antonio Rv <antoniorp1998@gmail.com>
2020-11-24 01:04:38 +0100

    [skip ci] prueba enesima vencel

--- api/link.js
+++ api/link.js
@@ -1,6 +1,7 @@
+/*
+
 const Cancion = require('./cancion');
 const Peticion = require('./peticion');
-
 
 const errorPeticion = {
     error: 400,
@@ -17,7 +18,6 @@ const todoBien = {
     mensaje: "Todo Bien"
 }
 module.exports = (req, res) => {
-    
     if(req.body){
         var datos = req.body;
         var cancion = new Cancion( datos.acousticness, datos.danceability, datos.energy, datos.instrumentalness, datos.liveness, datos.loudness, datos.mode, datos.popularity, datos.speechiness, datos.valence );
@@ -43,4 +43,10 @@ module.exports = (req, res) => {
         else{
             res.status(400).send(JSON.stringify(errorPeticion));
         }
+        
+    }
+*/
+module.exports = (req, res) => {
+    const { name = 'World' } = req.query
+    res.status(200).send(`Hello ${name}!`)
   }
